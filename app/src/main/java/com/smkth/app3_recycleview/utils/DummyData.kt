aeabff33e72bceb3334c19466fbc674750823a60
bramastamp <bramastamp.65@gmail.com>
2025-08-06 10:15:20 +0700

    feat: menambahkan 10 siswa ke RecyclerView

--- app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
+++ app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
@@ -7,7 +7,17 @@ object DummyData{
         return listOf(
             Student("Andi", "123456", "XII TJKT 1"),
             Student("Okta", "123456", "XII TJKT 1"),
-            Student("Soli", "123456", "XII TJKT 1")
+            Student("Soli", "123456", "XII TJKT 1"),
+            Student("Andika", "123456", "XII TJKT 1"),
+            Student("Andromeda", "123456", "XII TJKT 1"),
+            Student("Bramasta", "123456", "XII TJKT 1"),
+            Student("Marga", "123456", "XII TJKT 1"),
+            Student("Pangukuh", "123456", "XII TJKT 1"),
+            Student("Galang", "123456", "XII TJKT 1"),
+            Student("Surya", "123456", "XII TJKT 1"),
+            Student("Admaja", "123456", "XII TJKT 1"),
+            Student("Joko", "123456", "XII TJKT 1"),
+            Student("Widodo", "123456", "XII TJKT 1")
         )
     }
 }
